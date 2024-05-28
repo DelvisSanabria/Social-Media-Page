777bb60bf3c1362eb5202f9e7e1b7ee17de61835
delvis Sanabria <134345133+DelvisSanabria@users.noreply.github.com>
2024-05-28 14:53:42 -0400

    minor changes

--- Social-Media-Page/src/SocialMediaPage.jsx
+++ Social-Media-Page/src/SocialMediaPage.jsx
@@ -1,13 +1,18 @@
 import logoLexpin from "./assets/Lexpin_Logo-color.png";
+import darkLogoLexpin from "./assets/Lexpin_Logo-blanco.png";
 import SocialMedia from "./componets/SocialMedia";
 import iconLexpin from "./assets/lexpin_Icono-color.png";
+import { useMediaQuery } from "@uidotdev/usehooks";
 function SocialMediaPage() {
+  const isDarkMode = useMediaQuery("(prefers-color-scheme: dark)")
+
+  const logoSrc = isDarkMode ? darkLogoLexpin : logoLexpin;
   return (
     <>
       <section className="w-full py-8 md:py-5 lg:py-0 bg-no-repeat bg-cover bg-[url('./assets/grafiente.png')] dark:bg-gray-800 md:bg-[url('./assets/Wallpaper.png')]">
         <div className="grid items-center justify-center gap-4 px-4 text-center md:gap-1 md:px-6 lg:gap-6">
           <div className="space-y-3 grid grid-rows-2 place-items-center">
-            <img className="w-[282px]" src={logoLexpin} alt="" />
+            <img className="w-[282px]" src={logoSrc} alt="" />
             <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl/relaxed lg:text-xl/relaxed xl:text-xl/relaxed dark:text-gray-400">
               ¡No te pierdas de nada! Explora más contenido y dinámicas en
               nuestras redes sociales:
